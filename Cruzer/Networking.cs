d094059a3bac5f29374fbde4f73d3c5438566a7e
sp0rk1s <elis.rosengren@outlook.com>
2024-06-07 21:44:06 +0200

    modified:   Cruzer/Networking.cs 	modified:   Cruzer/bin/Debug/net8.0/Cruzer.dll 	modified:   Cruzer/bin/Debug/net8.0/Cruzer.exe 	modified:   Cruzer/bin/Debug/net8.0/Cruzer.pdb 	modified:   Cruzer/obj/Cruzer.csproj.nuget.dgspec.json 	modified:   Cruzer/obj/Debug/net8.0/Cruzer.AssemblyInfo.cs 	modified:   Cruzer/obj/Debug/net8.0/Cruzer.AssemblyInfoInputs.cache 	modified:   Cruzer/obj/Debug/net8.0/Cruzer.GeneratedMSBuildEditorConfig.editorconfig 	modified:   Cruzer/obj/Debug/net8.0/Cruzer.assets.cache 	modified:   Cruzer/obj/Debug/net8.0/Cruzer.dll 	modified:   Cruzer/obj/Debug/net8.0/Cruzer.genruntimeconfig.cache 	modified:   Cruzer/obj/Debug/net8.0/Cruzer.pdb 	modified:   Cruzer/obj/Debug/net8.0/apphost.exe 	modified:   Cruzer/obj/Debug/net8.0/ref/Cruzer.dll 	modified:   Cruzer/obj/Debug/net8.0/refint/Cruzer.dll 	modified:   Cruzer/obj/project.nuget.cache 	modified:   CruzerServer/Program.cs 	modified:   CruzerServer/bin/Debug/net8.0/CruzerServer.dll 	modified:   CruzerServer/bin/Debug/net8.0/CruzerServer.exe 	modified:   CruzerServer/bin/Debug/net8.0/CruzerServer.pdb 	modified:   CruzerServer/obj/Debug/net8.0/CruzerServer.AssemblyInfo.cs 	modified:   CruzerServer/obj/Debug/net8.0/CruzerServer.AssemblyInfoInputs.cache 	modified:   CruzerServer/obj/Debug/net8.0/CruzerServer.dll 	modified:   CruzerServer/obj/Debug/net8.0/CruzerServer.pdb 	modified:   CruzerServer/obj/Debug/net8.0/apphost.exe 	modified:   CruzerServer/obj/Debug/net8.0/ref/CruzerServer.dll 	modified:   CruzerServer/obj/Debug/net8.0/refint/CruzerServer.dll

--- Cruzer/Networking.cs
+++ Cruzer/Networking.cs
@@ -6,49 +6,26 @@ using System.Text;
 namespace Cruzer {
  
 class Networking {
-	
-	// ExecuteClient() Method
 	static void ExecuteClient()
 	{
-	
 		try {
 			IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
 			IPAddress ipAddr = ipHost.AddressList[0];
 			IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
-	
-			// Creation TCP/IP Socket using 
-			// Socket Class Constructor
-			Socket sender = new Socket(ipAddr.AddressFamily,
-					SocketType.Stream, ProtocolType.Tcp);
-	
+			Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 			try {
-				
-				// Connect Socket to the remote 
-				// endpoint using method Connect()
 				sender.Connect(localEndPoint);
+				if (sender.RemoteEndPoint == null) {
+                        Console.WriteLine("Socket connected to -> {0} ");
+                    } else {
+                        Console.WriteLine("Socket connected to -> {0} ", sender.RemoteEndPoint.ToString());
+                    }
 	
-				// We print EndPoint information 
-				// that we are connected
-				Console.WriteLine("Socket connected to -> {0} ", sender.RemoteEndPoint.ToString());
-	
-				// Creation of message that
-				// we will send to Server
 				byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
 				int byteSent = sender.Send(messageSent);
-	
-				// Data buffer
 				byte[] messageReceived = new byte[1024];
-	
-				// We receive the message using 
-				// the method Receive(). This 
-				// method returns number of bytes
-				// received, that we'll use to 
-				// convert them to string
 				int byteRecv = sender.Receive(messageReceived);
-				Console.WriteLine("Message from Server -> {0}", 
-					Encoding.ASCII.GetString(messageReceived, 
-												0, byteRecv));
-	
+				Console.WriteLine("Message from Server -> {0}", Encoding.ASCII.GetString(messageReceived, 0, byteRecv));
 				sender.Shutdown(SocketShutdown.Both);
 				sender.Close();
 			}
--- CruzerServer/obj/Debug/net8.0/CruzerServer.AssemblyInfo.cs
+++ CruzerServer/obj/Debug/net8.0/CruzerServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CruzerServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2489a01f0691d986db2b9d5f8d9ce101345e24c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CruzerServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CruzerServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
